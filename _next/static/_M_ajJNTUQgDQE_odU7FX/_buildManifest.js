bef4290942cedbdca96134b2778b4bc0800031d1
MichaelJuannn <juan600d@gmail.com>
2022-12-21 08:17:19 +0700

    modal

---
+++ _next/static/_M_ajJNTUQgDQE_odU7FX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-133454992f9199be.js"],"/_error":["static/chunks/pages/_error-0aba39871a1fa40a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
